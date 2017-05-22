f0890841a11520b1094eee2da2650f46b49aed5e
Mahmoud El Kotoury <melkotoury@gmail.com>
2017-05-22 20:11:50 +0200

    feat: add frontend validation

--- 01-todolist/js/script.js
+++ 01-todolist/js/script.js
@@ -1,5 +1,62 @@
 $(function() {
+        //Deadline Format
         $('#deadline').datetimepicker({
             format: 'DD-MM-YYYY HH:mm:ss'
         });
+        //   Edit and Create Validation
+
+
+    $('#updateForm').validate({
+        rules: {
+            title: {
+                required: true,
+                minlength: 3,
+                maxlength: 12
+            },
+            body: {
+                required: true,
+                minlength: 8,
+                maxlength: 100
+            },
+            priority: {
+                required: true
+            },
+            deadline: {
+                required: true,
+                datetime: true
+            }
+        }
+    });
+    $('#createForm').validate({
+        rules: {
+            title: {
+                required: true,
+                minlength: 3,
+                maxlength: 12
+            },
+            body: {
+                required: true,
+                minlength: 8,
+                maxlength: 100
+            },
+            priority: {
+                required: true
+            },
+            deadline: {
+                required: true,
+                datetime: true
+            }
+        }
+    });
+
+        // Style Errors in Validation
+        $('#title-error').css({
+                "color":"red",
+                "font-size":"0.9em",
+                "margin-left":"5px",
+                "float": "left"
+        });
+
+
+
 });
